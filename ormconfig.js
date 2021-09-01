0e27b7df0f64c4a53fb7ac2ab5bd95d5f8a5752f
yuri014 <yuriflores2012@gmail.com>
2021-08-31 21:46:15 -0300

    nest and orm config

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
