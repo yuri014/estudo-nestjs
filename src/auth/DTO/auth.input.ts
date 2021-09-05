69d5f50aa935067e5bdbb08ca4ca06c3074f3c6b
yuri014 <yuriflores2012@gmail.com>
2021-09-05 20:16:35 -0300

    adding auth DTOs

---
+++ src/auth/DTO/auth.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+@InputType()
+export class AuthInput {
+  @Field()
+  @IsEmail()
+  @IsNotEmpty({ message: 'Campo e-mail não pode ser vazio.' })
+  email: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty({ message: 'Campo senha não pode ser vazio.' })
+  password: string;
+}
