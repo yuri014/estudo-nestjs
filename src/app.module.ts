7101f3bcdba9eb58e9d5f788c09e2f4976eb7e8f
yuri014 <yuriflores2012@gmail.com>
2021-09-05 20:16:13 -0300

    adding auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UserModule } from './user/user.module';
       playground: true,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
