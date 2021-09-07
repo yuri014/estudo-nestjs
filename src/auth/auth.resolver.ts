8db9860599dc1f1751c09ce1ced43014f8e21cb0
yuri014 <yuriflores2012@gmail.com>
2021-09-06 21:35:58 -0300

    adding login resolver

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
+import { AuthService } from './auth.service';
+import { AuthInput } from './DTO/auth.input';
+import { AuthType } from './DTO/auth.type';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private authService: AuthService) {}
+
+  @Mutation(() => AuthType)
+  public async login(@Args('data') data: AuthInput): Promise<AuthType> {
+    const response = await this.authService.validateUser(data);
+
+    return {
+      user: response.user,
+      token: response.token,
+    };
+  }
+}
