2902700d8139dbc866afb163e60e463344eb4bfb
yuri014 <yuriflores2012@gmail.com>
2021-09-06 21:36:24 -0300

    returning jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { compareSync } from 'bcrypt';
 
+import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
 import { AuthInput } from './DTO/auth.input';
 import { AuthType } from './DTO/auth.type';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  private async jwtToken(user: User): Promise<string> {
+    const payload = { username: user.name, sub: user.id };
+
+    return this.jwtService.signAsync(payload);
+  }
 
   async validateUser(data: AuthInput): Promise<AuthType> {
     const user = await this.userService.findUserByEmail(data.email);
@@ -18,9 +29,11 @@ export class AuthService {
       throw new UnauthorizedException('Senha incorreta');
     }
 
+    const token = await this.jwtToken(user);
+
     return {
       user,
-      token: 'placeholder',
+      token,
     };
   }
 }
