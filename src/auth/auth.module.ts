1e1125225949e8835a2e85c58e2f47e5311e076b
yuri014 <yuriflores2012@gmail.com>
2021-09-06 21:36:11 -0300

    adding auth injections

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { UserService } from '../user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '30s' },
+      }),
+    }),
+  ],
+  providers: [AuthService, AuthResolver, UserService],
 })
 export class AuthModule {}
